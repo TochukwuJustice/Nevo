c593ef2075b97a962657e162c65ebe6d46c104f1
𝙳 𝙲𝙷𝙴𝙵 <onodairo449449@gmail.com>
2026-06-26 07:37:56 +0000

    add sortBy query param to GET /pools/:id/donations and GET /users/me/donations

--- nevo_server/src/donations/donations.module.ts
+++ nevo_server/src/donations/donations.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Donation } from './donation.entity.js';
 import { DonationsService } from './donations.service.js';
+import { DonationsController } from './donations.controller.js';
 import { ContractModule } from '../contract/contract.module.js';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Donation]), ContractModule],
   providers: [DonationsService],
+  controllers: [DonationsController],
   exports: [DonationsService],
 })
 export class DonationsModule {}
